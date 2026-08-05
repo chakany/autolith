47e33eb3f17bcce4e816f6aaa1ff8d2b1470680a
Jack Chakany <jack.chakany@braiins.cz>
2026-08-05 14:40:39 +0200

    Compile Autolith images as derivations in Nix

--- src/startup/active-image.lisp
+++ src/startup/active-image.lisp
@@ -127,8 +127,6 @@
                       (lisp-implementation-version))
              (string= (getf (rest record) :operating-system)
                       (software-type))
-             (string= (getf (rest record) :operating-system-version)
-                      (software-version))
              (string= (getf (rest record) :architecture)
                       (machine-type))
              (equal (mapcar #'first source-files)
