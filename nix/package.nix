7bc26579e143097a70e39bb38f20579bc035192a
Lukáš Hozda <luk.hozda@gmail.com>
2026-08-05 02:05:46 +0200

    Release Autolith 0.22.1

--- nix/package.nix
+++ nix/package.nix
@@ -195,7 +195,7 @@ let
 
   autolithSystem = pkgs.sbcl.buildASDFSystem {
     pname = "autolith";
-    version = "0.22.0";
+    version = "0.22.1";
     inherit src;
     systems = [ "autolith" "autolith/tests" ];
     lispLibs = with pkgs.sbclPackages; [
