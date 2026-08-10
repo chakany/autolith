8755dc516c9f6c1126b148d3fc426312095de871
Lukáš Hozda <luk.hozda@gmail.com>
2026-08-10 02:35:24 +0200

    Release 0.28.1

--- nix/package.nix
+++ nix/package.nix
@@ -242,7 +242,7 @@ let
 
   autolithSystem = pkgs.sbcl.buildASDFSystem {
     pname = "autolith";
-    version = "0.28.0";
+    version = "0.28.1";
     inherit src;
     systems = [ "autolith" "autolith/tests" ];
     lispLibs = with pkgs.sbclPackages; [
--- src/configuration/settings.lisp
+++ src/configuration/settings.lisp
@@ -2,7 +2,7 @@
 
 ;;;; -- Defaults --
 
-(defparameter *autolith-version* "0.28.0"
+(defparameter *autolith-version* "0.28.1"
   "The user-visible Autolith version.")
 
 (defparameter *default-model* "gpt-5.6-sol"
