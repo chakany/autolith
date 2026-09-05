892e223b0b7a2d7143787b7c9e4d7ecee644a83c
Lukáš Hozda <luk.hozda@gmail.com>
2026-09-05 20:41:54 +0200

    Prepare Autolith 0.47.1

--- nix/package.nix
+++ nix/package.nix
@@ -427,7 +427,7 @@ let
 
   autolithSystem = pkgs.sbcl.buildASDFSystem {
     pname = "autolith";
-    version = "0.47.0";
+    version = "0.47.1";
     inherit src;
     systems = [ "autolith" "autolith/tests" ];
     lispLibs = with pkgs.sbclPackages; [
