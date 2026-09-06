be630a064fe23f059c1d47a7a7d659ef2a4b57f8
Lukáš Hozda <luk.hozda@gmail.com>
2026-09-06 23:55:49 +0200

    Release 0.47.2

--- src/configuration/settings.lisp
+++ src/configuration/settings.lisp
@@ -2,7 +2,7 @@
 
 ;;;; -- Defaults --
 
-(defparameter *autolith-version* "0.47.1"
+(defparameter *autolith-version* "0.47.2"
   "The user-visible Autolith version.")
 
 (defparameter *default-model* "gpt-5.6-sol"
